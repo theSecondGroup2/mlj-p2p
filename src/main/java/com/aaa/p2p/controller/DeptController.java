d5ea09deff5a1fa5e414e002b64aeacc057081c3
aaaqy11 <928427598@qq.com>
2018-12-10 21:23:08 +0800

    2018.12.10 1.完成部门表增删改查

--- src/main/java/com/aaa/p2p/controller/DeptController.java
+++ src/main/java/com/aaa/p2p/controller/DeptController.java
@@ -1,11 +1,16 @@
 package com.aaa.p2p.controller;
 
 import com.aaa.p2p.service.DeptService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -37,5 +42,64 @@ public class DeptController {
     @RequestMapping("/toDept")
     public String toDept(){
         return "back/dept";
+    }
+    /**
+     *通过条件获取部门列表
+     * @return
+     */
+    @RequestMapping("/getDeptListBy")
+    @ResponseBody
+    public Object getDeptListBy(@RequestBody Map map){
+        //设置当前第几页和每页显示数量
+        PageHelper.startPage(Integer.valueOf(map.get("pageNo") + ""), Integer.valueOf(map.get("pageSize") + ""));
+        //用PageInfo对结果进行包装
+        PageInfo<Map> pageInfo = new PageInfo<Map>(deptService.getDeptListBy(map));
+        Map resultMap = new HashMap();
+        resultMap.put("total", pageInfo.getTotal());
+        resultMap.put("pageData", pageInfo.getList());
+        return resultMap;
+    }
+    /**
+     * 添加部門信息
+     * @param map
+     * @return
+     */
+    @RequestMapping("/insertAdd")
+    @ResponseBody
+    public Object insertAdd(@RequestBody Map map) {
+        System.out.println("添加的："+map);
+        return deptService.deptAdd(map);
+    }
+
+   /**
+    * 更改员工信息
+     * @param map
+     * @return
+             */
+    @RequestMapping("/updateDept")
+    @ResponseBody
+    public Object updateDept(@RequestBody Map map) {
+        System.out.println("更改的"+map);
+        return deptService.updateDept(map);
+    }
+
+    /**
+     * 删除
+     * @param map
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/delete")
+    public Object delete(@RequestBody Map map){
+        return deptService.deleteDept(Integer.valueOf(map.get("ID")+""));
+    }
+
+    /**
+     * 批量删除
+     */
+    @ResponseBody
+    @RequestMapping("/batchDel")
+    public Object batchDel(@RequestBody Map map){
+        return deptService.batchDel(map);
     }
 }
--- src/main/java/com/aaa/p2p/service/DeptServiceImpl.java
+++ src/main/java/com/aaa/p2p/service/DeptServiceImpl.java
@@ -4,6 +4,7 @@ import com.aaa.p2p.dao.DeptDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,5 +27,60 @@ public class DeptServiceImpl implements DeptService{
     public List<Map> getDeptList() {
         List<Map> deptList = deptDao.getDeptList();
         return deptList;
+    }
+    /**
+     * 通过条件获取部门列表
+     * @return
+     */
+    @Override
+    public List<Map> getDeptListBy(Map map) {
+        return deptDao.getDeptListBy(map);
+    }
+
+    /**
+     * 添加
+     * @param map
+     * @return
+     */
+    @Override
+    public int deptAdd(Map map) {
+        return deptDao.deptAdd(map);
+    }
+
+    /**
+     * 更新
+     * @param map
+     * @return
+     */
+    @Override
+    public int updateDept(Map map) {
+        return deptDao.updateDept(map);
+    }
+
+    /**
+     * 删除
+     * @param ID
+     * @return
+     */
+    @Override
+    public int deleteDept(Integer ID) {
+        return deptDao.deleteDept(ID);
+    }
+
+    /**
+     * 批量删除
+     * @param
+     * @return
+     */
+    @Override
+    public int batchDel(Map ids) {
+        String ides = ids.get("ids") + "";
+        String[] idArr = ides.split(",");
+        List IDs = new ArrayList();
+        for (int i = 0; i< idArr.length;i++) {
+            IDs.add(Integer.valueOf(idArr[i]));
+        }
+        int i =deptDao.batchDel(IDs);
+        return  i;
     }
 }
